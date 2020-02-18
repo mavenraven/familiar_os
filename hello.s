b26e4900a3a6070fa0ae4d88ed3407469b5c0ebe
mavenraven <developers@mavenraven.org>
2020-02-18 00:05:41 -0500

    more

--- hello.s
+++ hello.s
@@ -1,7 +1,18 @@
-mov al, 10 
+; http://www.techpository.com/wp-content/uploads/2015/07/examiningpartitiontables.pdf
+
+
+
+
+mov ah, 0x0a
+mov al, 0x54
+mov bh, 0x01
+mov cx, 0x01
+int 0x10
+
+
 
 ;pad with nops upto bootloader magic bytes
-%rep 508 
+%rep 499
 nop
 %endrep
 db 0x55
