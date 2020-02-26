7ac465cf3267c59dd77625e7bec9a5963f57b329
mavenraven <developers@mavenraven.org>
2020-02-25 22:55:04 -0500

    new information!

--- hello.s
+++ hello.s
@@ -24,6 +24,10 @@
 ;
 ; It turns out that this is just a convention though. The only thing the BIOS does is check for the magic 55aa bytes.
 ; If they're found, the PC is set to 0x00 and you're off to the races.
+;
+; NOT TRUE! Once your past the OEM name, it looks that you're not executable anymore.
+; Yep, that's verified. You can use the OEM space for instructions, but once you're in the
+; BIOS paramter block, you're done.
 
 %rep 4
 db 0x00
