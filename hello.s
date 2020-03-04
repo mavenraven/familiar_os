fbe902a1fc7908a80df8b46db4b014d2e725aaec
mavenraven <developers@mavenraven.org>
2020-03-04 00:24:11 -0500

    yeah, that fat stuff doesnt actually matter, this works

--- hello.s
+++ hello.s
@@ -1,28 +1,5 @@
 org 0x7c00
-; There is a standard format for the beginning of a binary file for bootup:
-; that looks like:
-; http://www.techpository.com/wp-content/uploads/2015/07/examiningpartitiontables.pdf
-; jmp instruction (3 bytes) ;
 jmp main
-db 0x00
-; 
-;oem name (8 bytes)
-%rep 8
-db 0x00
-%endrep
-; 
-;bios paramter block
-;https://en.wikipedia.org/wiki/BIOS_parameter_block#FAT12_/_FAT16
-%rep 25
-db 0x00
-%endrep
-; 
-;extended bios paramter block
-;https://en.wikipedia.org/wiki/BIOS_parameter_block#DOS_4.0_EBPB
-%rep 54
-db 0x00
-%endrep
-
 puts:
   pop cx
   pop bx
