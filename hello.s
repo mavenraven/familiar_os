9364dca675ee6332cc58ef3b5e54154070d0d638
mavenraven <developers@mavenraven.org>
2020-03-03 23:39:16 -0500

    hmm

--- hello.s
+++ hello.s
@@ -3,8 +3,7 @@ org 0x7c00
 ; that looks like:
 ; http://www.techpository.com/wp-content/uploads/2015/07/examiningpartitiontables.pdf
 ; jmp instruction (3 bytes) ;
-push hello
-jmp puts
+jmp main
 db 0x00
 ; 
 ;oem name (8 bytes)
@@ -24,13 +23,15 @@ db 0x00
 db 0x00
 %endrep
 
-hello: db 'hello world', 0
-
+hello: db 'hello world', 0x00
+main:
+  push hello
+  jmp puts
 puts:
   pop bx
 .begin:
   mov dl, [bx]
-  cmp dl, 0
+  cmp dl, 0x00
   je .end
   mov ah, 0x0e
   mov al, [bx]
@@ -38,6 +39,9 @@ puts:
   inc bx
   jmp .begin
 .end:
+
+
+
 
 times 0x1fe - ($ - $$) db 0
 db 0x55
