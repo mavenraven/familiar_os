14f714c0d0c67dda4711f65d0a3f2d591c7b8e9e
mavenraven <developers@mavenraven.org>
2024-01-10 14:01:07 -0500

    Adds ability to core dump qemu vm on os x. This is useful to ensure that our virtual network card actually has the string `helloworld` somewhere in memory.

--- Makefile
+++ Makefile
@@ -8,6 +8,11 @@ $(o).img: src/*.s
 clean:
 	rm -rf $(o).img
 
-.PHONY: run
-run: all
-	./run-qemu.sh $(o).img
+.PHONY: run-osx
+run-osx: all
+	./run-qemu-osx.sh $(o).img
+
+.PHONY: run-osx-coredump
+run-osx-coredump: all
+	./setup-qemu-for-osx-core-dumps.sh
+	./run-qemu-osx-coredump.sh $(o).img
---
+++ run-qemu-osx.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -eu
+killall -SIGKILL qemu-system-x86_64 || true
+qemu-system-x86_64 -drive file="$1",format=raw -serial stdio  -m 128 -display none -netdev user,id=net0,hostfwd=tcp::19920-:19920 -device e1000,netdev=net0 -object filter-dump,id=f1,netdev=net0,file="$HOME"/dump.pcap &
+QEMU_PID="$!"
+echo 'helloworld' | nc 127.0.0.1 19920 &
+sleep 1
+/bin/kill -9 "$QEMU_PID"
