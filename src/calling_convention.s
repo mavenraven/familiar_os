3581181b4f2d3bc582f5f185def0ca7a62caf538
mavenraven <developers@mavenraven.org>
2024-01-11 22:41:02 -0500

    fix spelling mistake

--- src/calling_convention.s
+++ src/calling_convention.s
@@ -1,6 +1,9 @@
+; This needs to match the number of registers saved on the stack during
+; the prologue.
 %define number_of_registers 5
+
 %define registers_space number_of_registers * 2
-%define return_addres_space 2
+%define return_address_space 2
 
 %macro prologue 0
   push ax
@@ -16,8 +19,8 @@
   ; The following is to put the return address
   ; at the top of the stack. This is so the caller
   ; doesn't have to do any cleanup.
-  mov cx, [esp + registers_space + return_addres_space]
-  mov     [esp + registers_space + return_addres_space + (%1 * 2)], cx
+  mov cx, [esp + registers_space + return_address_space]
+  mov     [esp + registers_space + return_address_space + (%1 * 2)], cx
 
   popfd
   pop dx
@@ -29,9 +32,9 @@
 %endmacro
 
 
-%define param1 esp + 2  + registers_space + return_addres_space
-%define param2 esp + 4  + registers_space + return_addres_space
-%define param3 esp + 6  + registers_space + return_addres_space
-%define param4 esp + 8  + registers_space + return_addres_space
-%define param5 esp + 10 + registers_space + return_addres_space
-%define param6 esp + 12 + registers_space + return_addres_space
+%define param1 esp + 2  + registers_space + return_address_space
+%define param2 esp + 4  + registers_space + return_address_space
+%define param3 esp + 6  + registers_space + return_address_space
+%define param4 esp + 8  + registers_space + return_address_space
+%define param5 esp + 10 + registers_space + return_address_space
+%define param6 esp + 12 + registers_space + return_address_space
