7b130cdbd83b6d8142e9f2763e4acf0dcb82dcde
mavenraven <developers@mavenraven.org>
2024-01-18 09:28:50 -0500

    yes, it's working!
    running against my debug version of qemu:
    No matching processes belonging to you were found
    RTL8139: Set IRQ to 0 (0000 0000)
    RTL8139: entered rtl8139_set_next_tctr_time
    drive: 0080
    load location: 7e00
    return code 0000
    sectors read: 0001
    face
    RTL8139 detected!
    device number: 0000
    0000
    c001
    c000
    c05e
    RTL8139: ioport read(w) addr=0x5e via read(b)
    RTL8139: PCI Revision ID read 0x20
    RTL8139: not implemented read(b) addr=0x5f
    RTL8139: ioport read(w) addr=0x5e val=0x0020
    0020
    
    0020 is the revision ID I see as well

--- src/main.s
+++ src/main.s
@@ -48,11 +48,69 @@ main:
   
   int 0x1a
 
+  ; read command register
   mov ah, PCI.PCI_FUNCTION_ID
   mov al, PCI.READ_CONFIG_WORD
-  mov di, 1
+  mov di, 4
 
   int 0x1a
 
+  ;command register, turn on the bus master bit
+  or cx, 4
+
+  ; then write it back 
+  mov ah, PCI.PCI_FUNCTION_ID
+  mov al, PCI.WRITE_CONFIG_WORD
+  mov di, 4
+
+  int 0x1a
+ 
+
+
+  ; we get the io base address similarly to the way this code does: http://www.jbox.dk/sanos/source/sys/dev/rtl8139.c.html
+  ; that code loops over each of the 6 base addresses and returns the first one that is an io address
+  ; 
+  ; we'll be lazy and just assume that the first base address is the io address
+
+  ; resources:
+  ; https://wiki.osdev.org/RTL8139
+  ; https://github.com/mavenraven/sanos/blob/master/src/sys/krnl/pci.c#L337-L354
+  ; https://github.com/mavenraven/sanos/blob/master/src/sys/dev/rtl8139.c#L1187
+  ; https://github.com/mavenraven/sanos/blob/master/src/sys/krnl/dev.c#L177-L182
+  ; https://github.com/mavenraven/sanos/blob/master/src/sys/krnl/dev.c#L159-L162
+
+
+  ; read second part of first BAR
+  mov ah, PCI.PCI_FUNCTION_ID
+  mov al, PCI.READ_CONFIG_WORD
+  mov di, 18
+  int 0x1a
+
   push cx
+  call putx
+
+  ; read first part of first BAR
+  mov ah, PCI.PCI_FUNCTION_ID
+  mov al, PCI.READ_CONFIG_WORD
+  mov di, 16
+  int 0x1a
+
+  push cx
+  call putx
+
+  ; on qemu the whole value is 0x0000c001. we bit shift 2 to the right because
+  ; the low 2 bits are reserved.
+
+  and cx, 0xFFFC
+  push cx
+  call putx
+
+ ; read from the address PCI Revision ID as a santity check
+  add cx, 0x005e
+  mov dx, cx
+  push dx
+  call putx
+
+  in ax, dx
+  push ax
   call putx
