7cfa237c8bb7a14715da0d3abce8c0f91cc4ebff
mavenraven <developers@mavenraven.org>
2024-01-19 23:19:04 -0500

    --wip-- [skip ci]

--- src/c/hello.c
+++ src/c/hello.c
@@ -1,11 +1,11 @@
 int hi(void);
 int main(void) {
    asm("mov $0x0E, %ah");
-   asm("mov $0x63, %al");
+   asm("mov $0x64, %al");
    asm("int $0x10");
 
    asm("mov $0x0, %dx");
-   asm("mov $0x63, %al");
+   asm("mov $0x64, %al");
    asm("mov $0x01, %ah");
    asm("int $0x14");
 
