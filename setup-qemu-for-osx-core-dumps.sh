3d89ebdab985a22c3affb516c7581c256764a867
mavenraven <developers@mavenraven.org>
2024-01-20 21:53:11 -0500

    --wip-- [skip ci]

--- setup-qemu-for-osx-core-dumps.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -eu
-
-# see https://nasa.github.io/trick/howto_guides/How-to-dump-core-file-on-MacOS.html
-sudo chmod 1777 /cores
-sudo sysctl kern.coredump=1
-rm segv.entitlements || true
-/usr/libexec/PlistBuddy -c "Add :com.apple.security.get-task-allow bool true" segv.entitlements
-codesign -s - -f --entitlements segv.entitlements `which qemu-system-x86_64` 
