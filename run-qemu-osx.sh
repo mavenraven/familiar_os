b8b11eb4ecb5d396e87a1701cb6e7d4bf2b1e5de
mavenraven <developers@mavenraven.org>
2024-01-20 21:39:07 -0500

    Revert "--wip-- [skip ci]"
    This reverts commit 3be87328cf22b8236ceb374c40dea46947425f07.

--- run-qemu-osx.sh
+++ run-qemu-osx.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -eu
 killall -SIGKILL qemu-system-x86_64 || true
-qemu-system-x86_64 -drive file="$1",format=raw -display none -serial stdio  -m 128 -netdev user,id=net0,hostfwd=tcp::19920-:19920 -device rtl8139,netdev=net0 -object filter-dump,id=f1,netdev=net0,file="$HOME"/dump.pcap &
+~/qemu/build/qemu-system-x86_64 -drive file="$1",format=raw -serial stdio  -m 128 -netdev user,id=net0,hostfwd=tcp::19920-:19920 -device rtl8139,netdev=net0 -object filter-dump,id=f1,netdev=net0,file="$HOME"/dump.pcap &
 QEMU_PID="$!"
 echo '295985df-c7e6-45fd-b1da-c3de476ad8d5' | nc 127.0.0.1 19920 &
 sleep 1
