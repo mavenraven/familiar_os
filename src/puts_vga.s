cecf06541a17eaeca32834f82219a49e644eace6
mavenraven <developers@mavenraven.org>
2024-01-21 01:16:51 -0500

    --wip-- [skip ci]

--- src/puts_vga.s
+++ src/puts_vga.s
@@ -9,25 +9,19 @@
 %endif
 
 puts_vga:
-   prologue
-;  push ax
-;  push ds
-;  push di
-;  push bx
+  push ax
+  push ds
+  push di
+  push bx
 
-;  mov ah, 0x00
-;  mov al, 0x03
-;  int 0x10
+  mov ax, 0xB800
+  mov ds, ax
+  mov di, 0x0778
+  mov bx, 0xFF30
+  mov [ds:di], bx
 
-;  mov ax, 0xB800
-;  mov ds, ax
-;  mov di, 0x0
-;  mov bx, 0xFF30
-;  mov [ds:di], bx
-
-;  pop bx
-;  pop di
-;  pop ds
-;  pop ax
-  epilogue 0
+  pop bx
+  pop di
+  pop ds
+  pop ax
   ret
