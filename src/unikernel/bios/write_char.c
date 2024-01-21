43fd15ad84f6d681208a8a2754523ac6d95efaf3
mavenraven <developers@mavenraven.org>
2024-01-20 21:39:02 -0500

    Revert "truly awful syntax"
    This reverts commit 2133e7a0d7ab0434cd5f2157e2f2825e54461337.

--- src/unikernel/bios/write_char.c
+++ src/unikernel/bios/write_char.c
@@ -1,23 +1,15 @@
 int write_char(int character) {
 	__asm__(
-		"mov $0x0E, %%ah\n"
-		"mov $0x64, %%al\n"
+		"mov $0x0E, %ah\n"
+		"mov $0x64, %al\n"
 		"int $0x10\n"
-
-		: 
-		:
-		: "%ah", "%al"
 	);
 
 	__asm__(
-		"mov $0x0, %%dx\n"
-		"mov $0x64, %%al\n"
-		"mov $0x01, %%ah\n"
+		"mov $0x0, %dx\n"
+		"mov $0x64, %al\n"
+		"mov $0x01, %ah\n"
 		"int $0x14\n"
-
-		: 
-		:
-		: "%dx", "%al", "%ah"
 	);
 
    return 0;
